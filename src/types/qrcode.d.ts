3c6e94ecdcc001e4eaddd44224f219ec54785819
LorenzaVolponi <79290340+LorenzaVolponi@users.noreply.github.com>
2026-08-24 13:26:48 -0300

    Add local QR code TypeScript declaration

---
+++ src/types/qrcode.d.ts
@@ -0,0 +1,17 @@
+declare module 'qrcode' {
+  interface QRCodeToDataURLOptions {
+    width?: number
+    margin?: number
+    color?: {
+      dark?: string
+      light?: string
+    }
+    errorCorrectionLevel?: 'L' | 'M' | 'Q' | 'H'
+  }
+
+  const QRCode: {
+    toDataURL(text: string, options?: QRCodeToDataURLOptions): Promise<string>
+  }
+
+  export default QRCode
+}
